36d7630e5eb2b6ac5f51f592babde5c3f0b80941
Teun <teun.nap@windesheim.nl>
2025-10-02 20:11:03 +0200

    update viewmodels

--- Grocery.App/ViewModels/BoughtProductsViewModel.cs
+++ Grocery.App/ViewModels/BoughtProductsViewModel.cs
@@ -24,7 +24,15 @@ namespace Grocery.App.ViewModels
 
         partial void OnSelectedProductChanged(Product? oldValue, Product newValue)
         {
-            //Zorg dat de lijst BoughtProductsList met de gegevens die passen bij het geselecteerde product. 
+            BoughtProductsList.Clear();
+            if (newValue is null)
+            {
+                return;
+            }
+            foreach (var bp in _boughtProductsService.Get(newValue.Id))
+            {
+                BoughtProductsList.Add(bp);
+            }
         }
 
         [RelayCommand]
