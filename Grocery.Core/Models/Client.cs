30ad463145237834ce5fda50e83f21d1cf3f8851
Teun <teun.nap@windesheim.nl>
2025-10-02 20:07:08 +0200

    Role toegevoegd aan client

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -1,10 +1,12 @@
 ﻿
 namespace Grocery.Core.Models
 {
+    public enum Role { None, Admin }
     public partial class Client : Model
     {
         public string EmailAddress { get; set; }
         public string Password { get; set; }
+        public Role Role { get; set; } = Role.None;
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
             EmailAddress=emailAddress;
